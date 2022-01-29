5a3b8c9bd1560a6f53d8ade1d35b94c7cb7fe5a5
Lilia <liliasemenova3@gmail.com>
2022-01-29 18:19:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log(leftBorderWidth);
+//console.log(second);
+var leftBorderWidth = 1;
+let second =2;
+const pi =3.14;
+console.log(leftBorderWidth);
+console.log(second);
+
+let arr=['plum.png','orange.jpg'];
+console.log(arr[0]);
