590dc46903632c5e15e49e2d35e79a33c7559c04
Lilia <liliasemenova3@gmail.com>
2022-02-01 18:57:46 +0200

    Lesson2.7

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 'use strict';
-console.log(leftBorderWidth);
-//console.log(second);
-var leftBorderWidth = 1;
-let second =2;
-const pi =3.14;
-console.log(leftBorderWidth);
-console.log(second);
 
-let arr=['plum.png','orange.jpg'];
-console.log(arr[0]);
+let money = prompt("Ваш бюджет на месяц?",'');
+let time = prompt("Введите дату в формате YYYY-MM-DD",'');
+let appData={
+    budget:money,
+    timeData:time,
+    expenses:{},
+    optionalExpenses :{},
+    income :[],
+    savings : false
+};
+let a1 = prompt("Введите обязательную статью расходов в этом месяце",''),
+    a2 = prompt("Во сколько обойдется?",''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце",''),
+    a4 = prompt("Во сколько обойдется?",'');
+//appData.expenses={a1,a2,a3,a4};
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+alert(appData.budget / 30);
